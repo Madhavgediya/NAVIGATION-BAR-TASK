156006a0a47a8bd5a5a6e86d5120c88df99ef8af
Madhavgediya <madhavgediya@gmail.com>
2023-06-19 20:20:26 +0530

    Javascript Math Method function, Random(), Boolean(), Comparisons, If else, Switch Case...

---
+++ ALL LANGUAGE/JAVASCRIPT/JAVASCRIPT IN W3SCHOOL/18 math.js
@@ -0,0 +1,28 @@
+document.getElementById("math").innerHTML = Math.PI;
+
+
+document.getElementById("math1").innerHTML = 
+"<p><b>Math.E:</b> " + Math.E + "</p>" +
+"<p><b>Math.PI:</b> " + Math.PI + "</p>" +
+"<p><b>Math.SQRT2:</b> " + Math.SQRT2 + "</p>" +
+"<p><b>Math.SQRT1_2:</b> " + Math.SQRT1_2 + "</p>" +
+"<p><b>Math.LN2:</b> " + Math.LN2 + "</p>" +
+"<p><b>Math.LN10:</b> " + Math.LN10 + "</p>" +
+"<p><b>Math.LOG2E:</b> " + Math.LOG2E + "</p>" +
+"<p><b>Math.Log10E:</b> " + Math.LOG10E + "</p>";
+
+document.getElementById("math2").innerHTML = Math.round(4.4);
+document.getElementById("math3").innerHTML = Math.ceil(4.4);
+document.getElementById("math4").innerHTML = Math.floor(5);
+
+document.getElementById("math5").innerHTML = Math.trunc(5);
+document.getElementById("math6").innerHTML = Math.sign(0);
+document.getElementById("math7").innerHTML = Math.pow(5,2);
+document.getElementById("math8").innerHTML = Math.sqrt(64);
+document.getElementById("math9").innerHTML = Math.abs(-45);
+document.getElementById("math10").innerHTML = Math.min(0,45,-19,12,-123,-334,123);
+document.getElementById("math11").innerHTML = Math.max(0,45,-19,12,-123,-334,123);
+document.getElementById("math12").innerHTML = Math.random();
+document.getElementById("math13").innerHTML = Math.log(10);
+document.getElementById("math14").innerHTML = Math.log2(32);
+
