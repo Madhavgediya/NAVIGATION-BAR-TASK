e586e442588b6591cf487d77f4544a7e1a91238c
Madhavgediya <madhavgediya@gmail.com>
2023-06-22 21:59:02 +0530

    JavaScript Today's Work Typeof (String, Number, Boolean, Null, undefine,...), Type Convertsion(Converting Strings to Numbers, Converting Numbers to Strings, Converting Dates to Numbers, Converting Numbers to Dates, Converting Booleans to Numbers, Converting Numbers to Booleans.), BitWise, Regular Expressions (Search, Replace), Operator Precedence, Scope (Block scope, Function scope, Global scope), Error...

---
+++ ALL LANGUAGE/JAVASCRIPT/JAVASCRIPT IN W3SCHOOL/25 typeof.js
@@ -0,0 +1,14 @@
+document.getElementById("demotypeof").innerHTML = 
+"Madhav is" + typeof "madhav" + "<br>" +
+"3.14 is" + typeof 3.14 + "<br>" +
+"NaN is" + typeof NaN + "<br>" +
+"False is" + typeof false + "<br>" +
+"[1,2,3,4] is" + typeof [1,2,3,4] + "<br>" +
+"{uname:'madhav', age:19} is" + typeof {uname:'madhav', age:19} + "<br>" +
+"Date() is" + typeof new Date()+ "<br>" +
+"function() is" + typeof function(){}+ "<br>" +
+"myCar is" + typeof myCar+ "<br>" +
+"null is" + typeof null + "<br>" ;
+
+
+
