4b6a7bd1cb4799a66475b74955bbbc81f6d11ad0
Madhavgediya <madhavgediya@gmail.com>
2023-06-29 18:33:23 +0530

    Javascript task 70% Completed

--- JAVASCRIPT NEW TASK/JS HTML/script.js
+++ JAVASCRIPT NEW TASK/JS HTML/script.js
@@ -1,92 +1,72 @@
-// Display Property Name ,Address And Price with its Symbol Make This Type Of Ui ,Creadted By Date
+// Display Property Name, Address And Price with its Symbol Make This Type Of Ui, Created By Date
 
 fetch("message.json")
-    .then(response => response.json())
-    .then(data => {
-        var result = data.result;
-        var datacontainer = document.getElementById("datacontainer");
-        
-        var countDisplay = document.getElementById("all");
-        var totalCount = result.length;
-        countDisplay.innerHTML = `All List ${totalCount}`;
-        var clickShowAll = document.getElementById("all");
-        clickShowAll.addEventListener("click",function (){
-            element(result);
-        })
+  .then(response => response.json())
+  .then(data => {
+    var result = data.result;
+    var datacontainer = document.getElementById("datacontainer");
 
-        var clickReview = document.getElementById("good");
-        var emptyReview = result.filter(function (result){
-            return result.property_reviews > 0;
-        }); 
-        clickReview.innerHTML = `Review ${emptyReview.length}`;
-        clickReview.addEventListener("click", function(){
-            element(emptyReview);
+    function makeButton(status, elementbtn, btnName) {
+      var countDisplay = document.getElementById(elementbtn);
+      if (status === "result") {
+        var totalCount = result.filter(function (element) {
+          return element.status += result.status;
         });
-
-        // var clickBadReview = document.getElementById("bad");
-        // var emptyBReview = result.filter(function (result){
-        //     return 0 < result.property_reviews;
-        // }); 
-
-        // clickBadReview.innerHTML = `Bad ${emptyBReview.length}`;
-        // clickBadReview.addEventListener("click", function(){
-        //     element(emptyBReview);
-        // });
-
-        
-        function dataput(good){
-            datacontainer.innerHTML = "";
-        
-        good.forEach(element => {
-            
-            var ratingStars = "";
-            var filledStars = element.property_reviews;
-            var emptyStars = 5 - filledStars;
-
-            for (var i = 0; i < filledStars; i++) {
-                ratingStars += '<i class="icon fa-solid fa-star"></i>';
-            }
-
-            for (var i = 0; i < emptyStars; i++) {
-                ratingStars += '<i class="icon fa-regular fa-star "></i>';
-            }
-
-            var images = document.createElement("div");
-            var date = element.createdAt;
-            images.innerHTML = `
-                <div class="row">
-                    <div class="col-3">            
-                        <div class="product">  
-                            <img class='img card-img-top' src="${element.image[element.image.length - 1]}">                    
-                            <div class="rupee">${element.price.map((e) => e.number + " " + e.currency.slice(0, 1))}</div>
-                            <div class="title">${element.title.slice(0, 15)}</div>
-                            <div class="city">${element.location_name}</div>             
-                            <hr class="m-auto">
-                            <div class="date"> ${date.slice(0, 10)}
-                            <span class="rating">Rate ${ratingStars}</span>
-                            </div>                    
-                        </div>
-                    </div>
-                </div>
-                `
-            datacontainer.appendChild(images);
+        countDisplay.innerHTML = `${btnName} (${totalCount.length})`;
+        countDisplay.addEventListener("click", function () {
+          dataput(totalCount.length);
         });
+      } else {
+        var totalCount = result.filter(function (element) {
+          return element.status === status;
+        });
+      }
+      countDisplay.innerHTML = `${btnName} (${totalCount.length})`;
+      countDisplay.addEventListener("click", function () {
+        dataput(totalCount);
+      });
     }
 
+    function dataput(item) {
+      datacontainer.innerHTML = "";
+
+      item.forEach(element => {
+        var ratingStars = "";
+        var filledStars = element.property_reviews;
+        var emptyStars = 5 - filledStars;
+
+        for (var i = 0; i < filledStars; i++) {
+          ratingStars += '<i class="icon fa-solid fa-star"></i>';
+        }
+
+        for (var i = 0; i < emptyStars; i++) {
+          ratingStars += '<i class="icon fa-regular fa-star "></i>';
+        }
+
+        var images = document.createElement("div");
+        var date = element.createdAt;
+        images.innerHTML = `
+        <div class="row">
+          <div class="col-lg-3">            
+            <div class="product">  
+              <img class='img card-img-top' src="${element.image[element.image.length - 1]}">                    
+              <div class="rupee">${element.price.map((e) => e.number + " " + e.currency.slice(0, 1))}</div>
+              <div class="title">${element.title.slice(0, 15)}</div>
+              <div class="city">${element.location_name}</div>             
+              <hr class="m-auto">
+              <div class="date"> ${date.slice(0, 10)}
+              <span class="rating">Rate ${ratingStars}</span>
+              </div>                    
+            </div>
+          </div>
+        </div>
+        `
+        datacontainer.appendChild(images);
+      });
+    }
+    makeButton("approved", "all", "Approved");
+    makeButton("pending", "bad", "Pending");
+    makeButton("decline", "good", "Decline");
+    makeButton("result", "all-product", "All");
     dataput(result);
-    });
-
-// function createList(data) {
-//           document.getElementById("m").src = data.result[0].image+"<br>";
-
-//         //     for(i in data.result){
-//         //     for(id in data.result[i])
-//         //     document.write(data.result  [i][id]+"<br>");
-//         //  }
-//         }
-
-// function make_filter() {
-//     // const
-// }
-
-// showList();
+  });
