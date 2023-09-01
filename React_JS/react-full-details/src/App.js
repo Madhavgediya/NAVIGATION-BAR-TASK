2eeadedfbcad3ed9c45909d0867be03c339c0822
Madhavgediya <madhavgediya@gmail.com>
2023-09-01 18:24:43 +0530

    Make tic-tac-toe 10%

---
+++ React_JS/react-full-details/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+let name = "Madhav Gediya";
+function App() {
+  return (
+    <>
+    <div>
+      this is <b>
+      {name}
+      </b>  
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis nemo veniam sint nisi animi, accusantium neque quisquam eius. Molestias, amet dolorem natus magni laborum saepe quasi nemo perferendis voluptates tempora? Illo temporibus, odio ullam asperiores numquam esse impedit ratione libero mollitia quidem commodi enim dignissimos aspernatur, quis nihil iste ut aut nulla ea quo hic! Beatae quo, odio nostrum temporibus minus doloribus exercitationem error ad, perferendis natus mollitia delectus explicabo quasi quam libero eum modi, quis rerum. Id nihil perspiciatis error corrupti repellat. Perferendis saepe vero illo fugit esse, illum quisquam veniam cumque impedit totam voluptatibus tempora recusandae. Natus, qui.
+      </p>
+      {/* <img src="{logo.svg}" alt="" /> */}
+    </div>
+    </>
+  );
+}
+
+export default App;
