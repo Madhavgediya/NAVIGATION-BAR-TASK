d98218195396924f84d7308ad6b8a2992eea3073
Madhavgediya <madhavgediya@gmail.com>
2023-10-31 18:22:26 +0530

    Fetch Other File Json Data in other one file, make search in website, information show with id

--- React_JS/testdemo/src/Component/Navbar.js
+++ React_JS/testdemo/src/Component/Navbar.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import JsonData from "../JsonData.json";
+import "../App.css";
 
-export default function Navbar() {
+
+export default function Navbar(props) {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg text-white bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+          <Link className="navbar-brand text-white" to="/">
             Data
           </Link>
           <button
@@ -18,67 +21,43 @@ export default function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className="text-white">
+            <i className="fa-solid fa-ellipsis"></i>
+            </span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div
+            className="collapse navbar-collapse justify-content-between"
+            id="navbarNav"
+          >
             <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link " to="/1">
-                  1
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/2">
-                  2
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/3">
-                  3
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/4">
-                  4
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link " to="/5">
-                  5
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/6">
-                  6
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/7">
-                  7
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link " to="/8">
-                  8
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/9">
-                  9
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link " to="/10">
-                  10
-                </Link>
-              </li>
+              {JsonData.map((element) => {
+                return (
+                  <li className="nav-item">
+                    <Link className="nav-link text-white" to={"/" + element.id}>
+                      {element.id}
+                    </Link>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
       </nav>
+      <form className="d-flex" role="search">
+        <input
+          className="form-control me-2 bg-dark text-white"
+          type="search"
+          placeholder="Search Here"
+          aria-label="Search"
+          onChange={props.change}
+        />
+      </form>
     </>
   );
 }
+
+
+
 
 
 
