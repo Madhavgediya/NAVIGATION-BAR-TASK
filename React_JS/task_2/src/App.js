90028417f4b7065a6e4af6341b9f31c154fb445d
Madhavgediya <madhavgediya@gmail.com>
2023-11-08 17:54:02 +0530

    send input field data one page to another page pass successfully in react js, use MUI desing like inputbox, button, alertbox, add new task

--- React_JS/task_2/src/App.js
+++ React_JS/task_2/src/App.js
@@ -2,33 +2,66 @@ import "./App.css";
 import { BrowserRouter as Main, Routes, Route } from "react-router-dom";
 import First from "./Components/First";
 import Second from "./Components/Second";
-import { useState } from "react";
+// import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("");
-  // const [print, setPrint] = useState("");
-  let nameReturn;
-  function inputBoxValue() {
-    let name = document.getElementById("inputBox").value;
-    console.log(name);
-    setName(name);
-    // return alert(name);
-    // setPrint(name);
-  }
   return (
     <>
-      <div>
-        {/* <Link to="/first"> */}
-        This is First Pages <br />
-        <input type="text" id="inputBox" onChange={inputBoxValue} />
-        <First />
-        <button href="" >Click me</button>
-        {/* </Link> */}
-        {/* <Second nam={name} /> */}
-      </div>
+      <Main>
+        <Routes>
+          <Route path="/" element={<First />} />
+          <Route path="/second" element={<Second />} />
+        </Routes>
+      </Main>
     </>
   );
 }
+
+// -------------------------------------------------------------------------------------------------------------------------------------------
+// function App() {
+//   return (
+//     <>
+//       <Main>
+//         <Routes>
+//           <Route path="/" element={<First />} />
+//           <Route path="/second/:nam" element={<Second />} />
+//         </Routes>
+//       </Main>
+//     </>
+//   );
+// }
+// -------------------------------------------------------------------------------------------------------------------------------------------
+// import "./App.css";
+// import { BrowserRouter as Main, Routes, Route } from "react-router-dom";
+// import First from "./Components/First";
+// import Second from "./Components/Second";
+// import { useState } from "react";
+
+// function App() {
+//   const [name, setName] = useState("");
+//   // const [print, setPrint] = useState("");
+//   let nameReturn;
+//   function inputBoxValue() {
+//     let name = document.getElementById("inputBox").value;
+//     console.log(name);
+//     setName(name);
+//     // return alert(name);
+//     // setPrint(name);
+//   }
+//   return (
+//     <>
+//       <div>
+//         {/* <Link to="/first"> */}
+//         This is First Pages <br />
+//         <input type="text" id="inputBox" onChange={inputBoxValue} />
+//         <First />
+//         <button href="" >Click me</button>
+//         {/* </Link> */}
+//         {/* <Second nam={name} /> */}
+//       </div>
+//     </>
+//   );
+// }
 
 export default App;
 
--- React_JS/task_2/src/Components/First.js
+++ React_JS/task_2/src/Components/First.js
@@ -1,42 +1,139 @@
-
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
 import Second from "./Second";
+// import { Hidden } from "@mui/material";
+
+// import Second from "./Second";
 
 function First() {
-  const [name, setName] = useState("");
+  const navigate = useNavigate();
+  const [name, setName] = useState("")
   let nameReturn;
   function inputBoxValue() {
     let name = document.getElementById("inputBox").value;
     console.log(name);
     setName(name);
-    return alert(name);
   }
+  // const textName = name;
+  function btnClick() {
+    alert(name);
+    navigate("/second", { state: { id: name } });
+  }
+
   return (
-    <div>
-    <Link to="/first">
-      This is First Pages <br />
-      <input type="text" id="inputBox" />
-      <button onClick={inputBoxValue}>Click me</button>
-      <Second nam={name} />
-    </Link>
-    </div>
+    <>
+      <div className="d-block justify-content-center align-items-center text-center">
+        <h2>
+          This is First Pages <br />
+        </h2>
+        <Paper
+          className="w-50"
+          component="form"
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            margin: "50px auto",
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Type Your Name & Go to Next Page"
+            inputProps={{ "aria-label": "search google maps" }}
+            id="inputBox"
+            onChange={inputBoxValue}
+          />
+          {/* <Link to={`/second/${<Second nam={textName}/>}`}> */}
+          {/* <Link to={'/second/'+{name}}> */}
+          <IconButton
+            type="button"
+            onClick={btnClick}
+            sx={{ p: "10px" }}
+            aria-label="next"
+          >
+            <SearchIcon />
+          </IconButton>
+          {/* </Link> */}
+        </Paper>
+        {/* <input type="text" id="inputBox" onChange={inputBoxValue} />
+      <Link to={`/second/${name}`}>
+        <button onClick={btnClick}>Click me</button>
+      </Link> */}
+      </div>
+    </>
   );
 }
 
 export default First;
 
+// -----------------------------------------------------------------------------------------------------------------------------------------
+// function First() {
+//   const [name, setName] = useState("");
+//   let nameReturn;
+//   function inputBoxValue() {
+//     let name = document.getElementById("inputBox").value;
+//     console.log(name);
+//     setName(name);
+//   }
+//   function btnClick() {
+//     return alert(name);
+//   }
+//   return (
+//     <>
+//       <div className="d-block justify-content-center align-items-center text-center">
+//         <h2>
+//           This is First Pages <br />
+//         </h2>
+//         <Paper
+//           className="w-50"
+//           component="form"
+//           sx={{ p: "2px 4px", display: "flex", alignItems: "center", justifyContent: "center", margin: "50px auto" }}
+//         >
+//           <InputBase
+//             sx={{ ml: 1, flex: 1 }}
+//             placeholder="Type Your Name & Go to Next Page"
+//             inputProps={{ "aria-label": "search google maps" }}
+//             id="inputBox"
+//             onChange={inputBoxValue}
+//           />
+//           <Link to={`/second/${name}`}>
+//             <IconButton
+//               type="button"
+//               onClick={btnClick}
+//               sx={{ p: "10px" }}
+//               aria-label="next"
+//             >
+//               <SearchIcon />
+//             </IconButton>
+//           </Link>
+//         </Paper>
+//         {/* <input type="text" id="inputBox" onChange={inputBoxValue} />
+//       <Link to={`/second/${name}`}>
+//         <button onClick={btnClick}>Click me</button>
+//       </Link> */}
+//       </div>
+//     </>
+//   );
+// }
 
+// export default First;
+// ------------------------------------------------------------------------------------------------------------------------------------------
 // import React from 'react'
 
 // function Demo() {
 //   return (
 //     <div>
-//       madhav 
+//       madhav
 //     </div>
 //   )
 // }
-
 
 // function Demo1() {
 //   return (
@@ -46,6 +143,4 @@ export default First;
 //   )
 // }
 
-// export {Demo1, Demo}; 
-
-
+// export {Demo1, Demo};
